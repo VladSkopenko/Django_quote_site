6670fbd05ddc239e804c125a7175c7ccd7770044
VladSkopenko <skopirka2k17@gmail.com>
2024-03-08 00:07:17 +0300

    save

---
+++ hw/quotes/templatetags/extract.py
@@ -0,0 +1,14 @@
+from django import template
+#from bson import ObjectId
+from ..utils import get_mongodb
+
+register = template.Library()
+
+
+def get_author(id_):
+    db = get_mongodb()
+    author = db.authors.find_one({"_id": str(id_)})
+    return author["fullname"]
+
+
+register.filter("author", get_author)
