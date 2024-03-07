7a748c859fcddafd20087dcd025e9da7523e0857
VladSkopenko <skopirka2k17@gmail.com>
2024-03-08 02:59:48 +0300

    added redis for up speed

--- hw/quotes/templatetags/extract.py
+++ hw/quotes/templatetags/extract.py
@@ -3,10 +3,14 @@ from bson.objectid import ObjectId
 from django import template
 
 from ..utils import get_mongodb
+from redis import StrictRedis
+from redis_lru import RedisLRU
 
 register = template.Library()
+client = StrictRedis(host="localhost", port=6379, password=None)
+cache = RedisLRU(client)
 
-
+@cache
 def get_author(id_):
     db = get_mongodb()
     author = db.authors.find_one({'_id': ObjectId(id_)})
