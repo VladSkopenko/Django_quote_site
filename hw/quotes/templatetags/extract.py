520fe77de4d048715c39263635cefdf062cbc69d
VladSkopenko <skopirka2k17@gmail.com>
2024-03-08 02:36:29 +0300

    WORKIN

--- hw/quotes/templatetags/extract.py
+++ hw/quotes/templatetags/extract.py
@@ -1,5 +1,7 @@
+from bson.objectid import ObjectId
+
 from django import template
-#from bson import ObjectId
+
 from ..utils import get_mongodb
 
 register = template.Library()
@@ -7,8 +9,8 @@ register = template.Library()
 
 def get_author(id_):
     db = get_mongodb()
-    author = db.authors.find_one({"_id": str(id_)})
-    return author["fullname"]
+    author = db.authors.find_one({'_id': ObjectId(id_)})
+    return author['fullname']
 
 
-register.filter("author", get_author)
+register.filter('author', get_author)
--- hw/utils/add_qoutes.py
+++ hw/utils/add_qoutes.py
@@ -1,7 +1,20 @@
 import json
+from bson.objectid import ObjectId
+
 from pymongo import MongoClient
 
+client = MongoClient('mongodb+srv://vladgo:1111@goitlearn.x6ks5fo.mongodb.net')
 
-client = MongoClient("mongodb+srv://vladgo:1111@goitlearn.x6ks5fo.mongodb.net/?retryWrites=true&w=majority")
 db = client.hw
 
+with open('quotes.json', 'r', encoding='utf-8') as fd:
+    quotes = json.load(fd)
+
+for quote in quotes:
+    author = db.authors.find_one({'fullname': quote['author']})
+    if author:
+        db.quotes.insert_one({
+            'tags': quote['tags'],
+            'author': ObjectId(author['_id']),
+            'quote': quote['quote']
+        })
