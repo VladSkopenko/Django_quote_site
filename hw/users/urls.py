0bcc372fa241a373bf21ecab597faefb742d4321
VladSkopenko <skopirka2k17@gmail.com>
2024-03-09 19:32:41 +0300

    fix urls

--- hw/users/urls.py
+++ hw/users/urls.py
@@ -9,5 +9,5 @@ app_name = "users"
 urlpatterns = [
     path("signup/", views.RegisterView.as_view(), name="signup"),
     path("signin/", LoginView.as_view(template_name="users/login.html", form_class=LoginForm), name="signin"),
-    path("logout/", LogoutView.as_view(), name="logout"),
+    path("logout/", LogoutView.as_view(template_name="users/logout.html"), name="logout"),
 ]
