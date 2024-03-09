039e21f44049314e272ea7cb00f618f97c36ae60
VladSkopenko <skopirka2k17@gmail.com>
2024-03-09 18:59:04 +0300

    adde pass and pass2

--- hw/users/forms.py
+++ hw/users/forms.py
@@ -1,14 +1,14 @@
-from django.forms import  CharField, ImageField, TextInput, FileInput
+from django.forms import CharField, TextInput, EmailInput, EmailField, PasswordInput
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 
 
-
-
 class RegisterForm(UserCreationForm):
-    username = CharField(max_length=19, min_length=2, required=True, widget=TextInput(attrs={"class": "form-control",}))
-
+    username = CharField(max_length=19, required=True, widget=TextInput(attrs={"class": "form-control", }))
+    email = EmailField(max_length=30, required=True, widget=EmailInput(attrs={"class": "form-control", }))
+    password1 = CharField(required=True, widget=PasswordInput(attrs={"class": "form-control", }))
+    password2 = CharField(required=True, widget=PasswordInput(attrs={"class": "form-control", }))
 
     class Meta:
         model = User
-        fields = ["description", "path"]
+        fields = ("username", "email", "password1", "password2")
