d4d712b28cff4725d0fc231dbedc60576d7bb3b3
VladSkopenko <skopirka2k17@gmail.com>
2024-03-09 19:31:07 +0300

    update urls

--- hw/users/urls.py
+++ hw/users/urls.py
@@ -2,11 +2,12 @@ from django.urls import path
 from django.contrib.auth.views import LoginView, LogoutView
 
 from . import views
+from .forms import LoginForm
 
 app_name = "users"
 
 urlpatterns = [
-    path("signup/", views.main, name="signup"),
-    path("signin/", LoginView.as_view(), name="signin"),
+    path("signup/", views.RegisterView.as_view(), name="signup"),
+    path("signin/", LoginView.as_view(template_name="users/login.html", form_class=LoginForm), name="signin"),
     path("logout/", LogoutView.as_view(), name="logout"),
 ]
