7f6afae041b3d4b4fdd4b44683358bd645007db1
VladSkopenko <skopirka2k17@gmail.com>
2024-03-09 17:38:54 +0300

    Create templates for users

--- hw/users/views.py
+++ hw/users/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
-# Create your views here.
+from django.views import View
+
+
+
+class RegisterView(View):
+    def get(self):
+        pass
+
+    def post(self, request):
+        pass
