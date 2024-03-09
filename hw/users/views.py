b12f39c48e16a9ed4e42fe13f430dbd51dd9a605
VladSkopenko <skopirka2k17@gmail.com>
2024-03-09 21:42:50 +0300

    fix user users!!!!

--- hw/users/views.py
+++ hw/users/views.py
@@ -18,7 +18,7 @@ class RegisterView(View):
             form.save()
             username = form.cleaned_data["username"]
             messages.success(request, f"Hello {username}, Your account created successfully")
-            return redirect(to="user:login")
+            return redirect(to="users:login")
         return render(request, self.template_name, context={"form": form})
 
 
