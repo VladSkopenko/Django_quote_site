1b9b6489a3ee37d017a76f9db2d44f2d80815922
VladSkopenko <skopirka2k17@gmail.com>
2024-03-10 16:12:54 +0300

    added button profile in the base.thml

--- hw/hw/urls.py
+++ hw/hw/urls.py
@@ -6,5 +6,5 @@ from django.urls import path, include
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("quotes.urls")),
-    path("users/", include("users.urls")),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    path('users/', include('users.urls')),
+    ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
