d2aac1efbefdecceef921d816a77db185f1aff9c
VladSkopenko <skopirka2k17@gmail.com>
2024-03-10 16:27:19 +0300

    + urls

--- hw/users/urls.py
+++ hw/users/urls.py
@@ -8,7 +8,8 @@ app_name = "users"
 
 urlpatterns = [
     path("register/", views.RegisterView.as_view(), name="register"),
-    path("login/", LoginView.as_view(template_name="users/login.html", form_class=LoginForm), name="login"),
+    path("login/", LoginView.as_view(template_name="users/login.html", form_class=LoginForm,
+         redirect_authenticated_user=True), name="login"),
     path("logout/", LogoutView.as_view(template_name="users/logout.html"), name="logout"),
     path('profile/', views.profile, name='profile'),
 ]
