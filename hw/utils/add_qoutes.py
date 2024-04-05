895bcaa51961f54757e17b9dcaaa5fa7c1d101e2
VladSkopenko <skopirka2k17@gmail.com>
2024-04-05 19:08:02 +0300

    hide mote

--- hw/utils/add_qoutes.py
+++ hw/utils/add_qoutes.py
@@ -1,20 +1,26 @@
 import json
-from bson.objectid import ObjectId
+import os
 
+from bson.objectid import ObjectId
+from dotenv import load_dotenv
 from pymongo import MongoClient
 
-client = MongoClient('mongodb+srv://vladgo:1111@goitlearn.x6ks5fo.mongodb.net')
+load_dotenv()
+mongo_uri = os.getenv("MONGO_URI")
 
+client = MongoClient(mongo_uri)
 db = client.hw
 
-with open('quotes.json', 'r', encoding='utf-8') as fd:
+with open("quotes.json", "r", encoding="utf-8") as fd:
     quotes = json.load(fd)
 
 for quote in quotes:
-    author = db.authors.find_one({'fullname': quote['author']})
+    author = db.authors.find_one({"fullname": quote["author"]})
     if author:
-        db.quotes.insert_one({
-            'tags': quote['tags'],
-            'author': ObjectId(author['_id']),
-            'quote': quote['quote']
-        })
+        db.quotes.insert_one(
+            {
+                "tags": quote["tags"],
+                "author": ObjectId(author["_id"]),
+                "quote": quote["quote"],
+            }
+        )
