8633a7d1261a2ca745687c892c6c233b3a1773bb
VladSkopenko <skopirka2k17@gmail.com>
2024-09-19 17:53:08 +0300

    fix conf

--- hw/quotes/views.py
+++ hw/quotes/views.py
@@ -24,7 +24,7 @@ def main(request, page=1):
     per_page = 10
     paginator = Paginator(quotes, per_page)
     quotes_on_page = paginator.page(page)
-    top_tags = Tag.objects.annotate(num_quotes=Count('quote')).order_by('-num_quotes')[:10]
+    top_tags = get_top_10_tags()
     return render(request, "quotes/index.html", context={"quotes": quotes_on_page, 'paginator': paginator,
                                                          'top_tags': top_tags})
 
